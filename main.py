d7f20d067ca3c8b93a41fd9a60b6dd00b3fcf5b1
relayparisjamaat <relay.parisjamaat@gmail.com>
2026-01-14 23:16:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def healthcheck():
 # --------------------------------------------------
 # Approval workflow
 # --------------------------------------------------
-@app.post("/approval")
+@app.api_route("/approval", methods=["POST", "GET"])
 async def jotform_approval(request: Request):
     data = await request.form()
     print("Jotform webhook keys:", list(data.keys()))
